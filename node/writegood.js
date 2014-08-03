f5a305f5ccbb3b8f53f8b51042fe4966a9630c49
Tony Meng <tony@firebase.com>
2014-08-03 11:44:40 -0700

    Adding multi-tenancy

--- node/writegood.js
+++ node/writegood.js
@@ -4,9 +4,24 @@ var Firebase = require('firebase'),
 
 var fbRef = new Firebase('https://write-good.firebaseio.com/');
 
-fbRef.child('input').on('value', function (snapshot) {
-  async.each([snapshot.val()], function (input, callback) {
-    fbRef.child('output').set(writeGood(input));
-    callback();
+var listeners = {};
+var listenToSession = function (sessionId) {
+  var sessionRef = fbRef.child('text').child(sessionId);
+  var listener = sessionRef.child('input').on('value', function (snapshot) {
+    async.each([snapshot.val()], function (input, callback) {
+      sessionRef.child('output').set(writeGood(input), function () {
+        callback();
+      });
+    });
   });
+  listeners[sessionId] = listener;
+};
+
+fbRef.child('text').on('child_added', function (snapshot) {
+  listenToSession(snapshot.name());
+});
+
+fbRef.child('text').on('child_removed', function (snapshot) {
+  var sessionId = snapshot.name();
+  fbRef.child(sessionId).off('value', listeners[sessionId]);
 });
